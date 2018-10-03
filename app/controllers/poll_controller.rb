848f183d84ec7367c177ff33963344439efd6256
Darnita <darnita.holmes@gmail.com>
2018-10-03 16:14:22 -0400

    updated routes

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -12,5 +12,6 @@ class PollController < ApplicationController
     redirect_to option_index_url
   end
 
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'poll/index'
+  post 'poll/create'
+  root 'poll#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
