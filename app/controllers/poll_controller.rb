2449e7f90c1ab859a907508abf6e52c3d6d71bb6
Darnita <darnita.holmes@gmail.com>
2018-10-03 16:03:04 -0400

    finished controller

---
+++ app/controllers/poll_controller.rb
@@ -0,0 +1,16 @@
+class PollController < ApplicationController
+  def index
+    @option = Option.all
+  end
+
+  def create
+    @option = Option.new
+    #these are columns
+    @option.name = params["name"]
+    @option.tally = params["tally"]
+    @option.save
+    redirect_to option_index_url
+  end
+
+
+end
