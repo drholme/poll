863b100fca5a3689b336d5991fd57364fc8af0ed
Darnita <darnita.holmes@gmail.com>
2018-10-04 11:27:13 -0400

    it works! sorta - fixed missing tags an dadded the checkmark funtionality

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -7,11 +7,21 @@ class PollController < ApplicationController
     @option = Option.new
     #these are columns
     @option.name = params["name"]
-    @option.tally = params["tally"]
+    @option.tally = 0
     @option.save
     redirect_to poll_index_url
   end
 
+  def vote
+    @option = Option.find(params[:id])
+    #these are columns
+    # @option.name = params["name"]
+    # @option.tally = params["tally"]
+    @option.tally += 1
+    @option.save
+    redirect_to poll_index_url
+    puts @option.tally
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'poll/index'
   post 'poll/create'
   root 'poll#index'
+  post 'poll/vote'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
